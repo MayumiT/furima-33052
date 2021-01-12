b5c2d61c19f8f8c1748834487bec4bf65905c0c6
MayumiT <thevirginmary1016@gmail.com>
2021-01-12 19:21:10 +0900

    factorybotの修正
    本名情報、誕生日を追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
     email                 {"test@example"}
     password              {"000000"}
     password_confirmation {password}
+    family_name           {"山田"}
+    family_name_kana      {"ヤマダ"}
+    first_name            {"太郎"}
+    first_name_kana       {"タロウ"}
+    birth                 {"2000-01-01"}
   end
 end
