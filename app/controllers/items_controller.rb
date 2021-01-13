187533fdaa3e1a9d0325b54b2417fe6cf49bf42f
MayumiT <thevirginmary1016@gmail.com>
2021-01-13 11:43:05 +0900

    不要なアクション削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,12 +5,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to root_path
-    else
-      render :new
-    end
   end
 
   def destroy
