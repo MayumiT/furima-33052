d347e70a1433116cdc0af50aa8bc85f350fccd7a
MayumiT <thevirginmary1016@gmail.com>
2021-01-14 15:03:20 +0900

    Itemモデル・テーブル作成

---
+++ db/migrate/20210114033647_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name,           null: false
+      t.string :text,           null: false
+      t.string :category_id,    null: false
+      t.string :condition_id,   null: false
+      t.string :prefecture_id,  null: false
+      t.string :area_id,        null: false
+      t.string :date_id,        null: false
+      t.string :price,          null: false
+      t.references :user,       foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
