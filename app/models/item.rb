299a90e01259a2357801b1d5811f7828f82f0f33
MayumiT <thevirginmary1016@gmail.com>
2021-01-14 15:26:23 +0900

    Itemモデルのアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+  has_one :buy
+  has_one_active_hash :category
+  has_one_active_hash :condition
+  has_one_active_hash :prefecture
+  has_one_active_hash :area
+  has_one_active_hash :date
 end
