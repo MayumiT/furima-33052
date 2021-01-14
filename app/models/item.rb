5f4b217faf5dd30a31f3be7897991c73bf1d8b05
MayumiT <thevirginmary1016@gmail.com>
2021-01-14 15:46:54 +0900

    Itemモデルのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,15 @@ class Item < ApplicationRecord
   has_one_active_hash :prefecture
   has_one_active_hash :area
   has_one_active_hash :date
+
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :category_id
+    validates :condition_id
+    validates :prefecture_id
+    validates :area_id
+    validates :date_id
+    validates :price
+  end
 end
