a9d3a31cde1ea6e39cf5261970e53fd5392b366a
MayumiT <thevirginmary1016@gmail.com>
2021-01-23 16:45:18 +0900

    修正（ordersコントローラー・モデル（address/area）・orders/index・ER図）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,7 +18,6 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    #params.permit(:item_id).merge(user_id:current_user.id)
-    params.require(:order_address).permit(:user_id, :item_id, :post_code, :area_id, :city, :house_number, :building_name, :phone_number, :order_id)
+    params.require(:order_address).permit(:post_code, :area_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id] )
   end
 end
