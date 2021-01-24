2c8103eadfd1cadc1dd07fedb3f9f7813180a60c
MayumiT <thevirginmary1016@gmail.com>
2021-01-25 02:48:03 +0900

    order_address単体テスト・モデル修正

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -8,6 +8,8 @@ FactoryBot.define do
     building_name       { '大阪aテスト１' }
     token               {"tok_abcdefghijk00000000000000000"}
 
+    user_id             {1}
+    item_id             {1}
     association :user
     association :item
   end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe OrderAddress, type: :model do
   describe '購入情報の保存' do
     before do
-      @user = FactoryBot.build(:user)
-      @item = FactoryBot.build(:item)
-      @order_address = FactoryBot.build(:order_address, user_id: @user.id, item_id: @item.id)
+      user = FactoryBot.build(:user)
+      item = FactoryBot.build(:item)
+      @order_address = FactoryBot.build(:order_address, user_id: user, item_id: item)
     end
     
     context '商品の購入ができる時' do
@@ -16,9 +16,19 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.building_name = ""
         expect(@order_address).to be_valid
       end
-    end 
+    end
     
     context '商品の購入ができない時' do
+      it 'user_idが空だと登録できない' do
+        @order_address.user_id = ""
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("User can't be blank")
+      end
+      it 'item_idが空だと登録できない' do
+        @order_address.item_id = ""
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
+      end
       it '郵便番号が空では、商品の購入ができない' do
         @order_address.post_code = ""
         @order_address.valid?
